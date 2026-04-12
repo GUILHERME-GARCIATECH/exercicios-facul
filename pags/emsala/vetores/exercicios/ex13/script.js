cc2b3fb8317054985b67665eafe1e214d541cd08
GUILHERME-GARCIATECH <guilherme.garciatech@gmail.com>
2026-04-12 14:50:00 -0400

    ex13-vetores-emsala

--- pags/emsala/vetores/exercicios/ex13/script.js
+++ pags/emsala/vetores/exercicios/ex13/script.js
@@ -6,7 +6,6 @@ function calcular() {
     let contMulheres = 0;
     let contHomens = 0; 
     
-   
     for(let i = 0; i < n; i++){
         let altura = Number(window.prompt(`Altura da ${i+1}ª pessoa`));
         let genero = window.prompt(`Genero da ${i+1}ª pessoa`);
@@ -20,7 +19,15 @@ function calcular() {
             contHomens++;
         }
     }
-    const mediaMulheres = contMulheres > 0 ? (somaAlturaMulheres / contMulheres).toFixed(2) : 0;
+
+    //const mediaMulheres = contMulheres > 0 ? (somaAlturaMulheres / contMulheres).toFixed(2) : 0;
+    let mediaMulheres;
+    if (contMulheres > 0) {
+        mediaMulheres = (somaAlturaMulheres / contMulheres).toFixed(2); 
+    }else {
+        mediaMulheres = 0;
+    }
+
     let maiorAltura = alturas[0];
     let Pmaior = 0;
 
@@ -39,5 +46,9 @@ function calcular() {
             Pmenor = i;
         }
     }
+    res.innerHTML = `<p>MEDIA DAS ALTURAS DAS MULHERES = ${mediaMulheres}</p>`;
+    res.innerHTML += `<p>MAIOR ALTURA = ${maiorAltura} (Pessoa ${Pmaior + 1})</p>`;
+    res.innerHTML += `<p>MENOR ALTURA = ${menorAltura} (Pessoa ${Pmenor + 1})</p>`;
+    res.innerHTML += `<p>QUANTIDADE DE HOMENS = ${contHomens}</p>`;
     
 }
--- pags/emsala/vetores/exercicios/ex14/script.js
+++ pags/emsala/vetores/exercicios/ex14/script.js
@@ -1,24 +1,37 @@
 function calcular() {
     let res = document.querySelector('div#res');
-    let n = Number(window.prompt("Quantos elementos vai ter o vetor?"))
-    let v = [];
-    let s = 0;
+    res.innerHTML = '';
+    let n = Number(window.prompt("Serão digitados dados de quantos produtos?"));
+    let somaCompra = 0;
+    let somaVenda = 0;
+    let lucro = 0;
+    let cont10 = 0;
+    let cont10a20 = 0;
+    let cont20 = 0
 
     for(let i = 0; i < n; i++){
-        let n = Number(window.prompt(`Informe o ${i+1}`));
+        window.alert(`Produto ${i+1}`);
+        let nome = window.prompt("Nome:")
+        let compra = Number(window.prompt("Preço de compra:"));
+        let venda = Number(window.prompt("Preço de venda:"));
+        somaCompra += compra;
+        somaVenda += venda;
 
-        if(n % 2 === 0){
-            v.push(n);
-            s += n;
+        const perLucro = (venda-compra) / compra
+        if(perLucro < 0.1){
+            cont10 += 1;
+        }else if(perLucro >= 0.1 && perLucro <= 0.2){
+            cont10a20 += 1;
+        }else{
+            cont20 += 1;
         }
 
+        lucro += (venda-compra);
     }
-    if(v.length == 0){
-        res.innerHTML = `<p>NENHUM NUMERO PAR</p>`
-    }else{
-        let m = s/v.length;
-        res.innerHTML = `<p>MEDIA DOS PARES = ${m.toFixed(1)}</p>`
-    }
-    
-
+    res.innerHTML += `<p>Lucro abaixo de 10%: ${cont10}</p>`;
+    res.innerHTML += `<p>lucro entre 10% e 20%: ${cont10a20}</p>`;
+    res.innerHTML += `<p>lucro maior que 20%: ${cont20}</p>`;
+    res.innerHTML += `<p>Valor total de compra: R$${somaCompra.toFixed(2)}</p>`;
+    res.innerHTML += `<p>Valor total de venda: R$${somaVenda.toFixed(2)}</p>`;
+    res.innerHTML += `<p>Lucro total: R$${lucro.toFixed(2)}</p>`;
 }
