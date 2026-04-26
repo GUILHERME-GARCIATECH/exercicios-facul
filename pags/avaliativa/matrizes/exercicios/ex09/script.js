5f3862a821182bd0eaad731fd246727e63f605be
Guilherme Garcia <guilherme.garciatech@gmail.com>
2026-04-25 21:20:10 -0400

    Ex 09 e 10 de matrizes

--- pags/avaliativa/matrizes/exercicios/ex09/script.js
+++ pags/avaliativa/matrizes/exercicios/ex09/script.js
@@ -1,11 +1,42 @@
+import { gerarMatrizAleatoria } from "../util.js";
+
 function calcular() {
-    let res = document.querySelector('div#res');
-    const v1 = [1, 2, 3];
-    const v2 = [4, 5, 6];
+  let res = document.querySelector("div#res");
+  res.innerHTML = "";
 
-    const v3 = [...v1, ...v2];
+  const matriz = gerarMatrizAleatoria(2, 3, 10);
+  const matrizTransposta = [];
 
-    res.innerHTML = `<p>Primeiro vetor = [${v1.join(', ')}]</p>
-    <p>Segundo vetor = [${v2.join(', ')}]</p>
-    <p>Vetor resultante = [${v3.join(', ')}]</p>`
+  //Exibe e transpõe a tabela original
+  let tabelaTrans = `<table class="matriz-table">`;
+  for (let i = 0; i < matriz[0].length; i++) {
+    matrizTransposta[i] = [];
+    tabelaTrans += `<tr>`;
+    for (let j = 0; j < matriz.length; j++) {
+      matrizTransposta[i][j] = matriz[j][i];
+
+      tabelaTrans += `<td>${matrizTransposta[i][j]}</td>`;
+    }
+    tabelaTrans += `</tr>`;
+  }
+  tabelaTrans += `</table>`;
+
+  //exibe a tabela original
+  let tabelaOrg = `<table class="matriz-table">`;
+  for (let i = 0; i < matriz.length; i++) {
+    tabelaOrg += `<tr>`;
+    for (let j = 0; j < matriz[i].length; j++) {
+      tabelaOrg += `<td>${matriz[i][j]}</td>`;
+    }
+    tabelaOrg += `</tr>`;
+  }
+
+  tabelaOrg += `</table>`;
+  res.innerHTML += `<p>Tabela original:</p>`;
+  res.innerHTML += tabelaOrg;
+
+  res.innerHTML += `<p>Tabela transposta:</p>`;
+  res.innerHTML += tabelaTrans;
 }
+
+window.calcular = calcular;
--- pags/avaliativa/matrizes/exercicios/ex10/script.js
+++ pags/avaliativa/matrizes/exercicios/ex10/script.js
@@ -1,16 +1,61 @@
 function calcular() {
-    let res = document.querySelector('div#res');
+  res.innerHTML = ``;
 
-    const v1 = [1, 2, 2, 3, 4, 4, 4, 5];
+  const matriz = [
+    ["Livre", "Livre", "Livre", "Livre", "Livre"],
+    ["Livre", "Livre", "Livre", "Livre", "Livre"],
+    ["Livre", "Livre", "Livre", "Livre", "Livre"],
+    ["Livre", "Livre", "Livre", "Livre", "Livre"],
+    ["Livre", "Livre", "Livre", "Livre", "Livre"],
+  ];
 
-    let v2 = [];
-
-    for (let i = 0; i < v1.length; i++) {
-        if (!v2.includes(v1[i])) {
-            v2.push(v1[i])
-        }
+  while (true) {
+    let l = Number(window.prompt("Linha (1-5). Zero ou negativo para sair:"));
+    if (l <= 0) {
+      alert("Programa finalizado!");
+      break;
+    }
+    if (l > 5) {
+      alert("Posição inválida! Escolha de 1 a 5.");
+      continue;
     }
 
-    res.innerHTML = `<p>Vetor principal  = [${v1.join(', ')}]</p>
-    <p>Vetor sem repetição = [${v2.join(', ')}]</p>`
+    let c = Number(window.prompt("Coluna (1-5). Zero ou negativo para sair:"));
+    if (c <= 0) {
+      alert("Programa finalizado!");
+      break;
+    }
+    if (c > 5) {
+      alert("Posição inválida! Escolha de 1 a 5.");
+      continue;
+    }
+
+    l -= 1;
+    c -= 1;
+
+    if (matriz[l][c] === "Livre") {
+      matriz[l][c] = "Reservada";
+      alert("Reserva realizada com sucesso!");
+    } else {
+      alert("Poltrona já está ocupada!");
+    }
+  }
+  exibirMatriz(matriz);
+}
+
+function exibirMatriz(matriz) {
+  let res = document.querySelector("div#res");
+  let tabelaHTML = `<table class="poltrona-table">`;
+
+  for (let i = 0; i < matriz.length; i++) {
+    tabelaHTML += `<tr>`;
+    for (let j = 0; j < matriz[i].length; j++) {
+      let classe = matriz[i][j] === "Reservada" ? "poltrona-reservada" : "poltrona-livre";
+      tabelaHTML += `<td class="${classe}">${matriz[i][j]}<br> <span style="font-size:0.6rem; opacity:0.6">${i},${j}</span></td>`;
+    }
+    tabelaHTML += `</tr>`;
+  }
+
+  tabelaHTML += `</table>`;
+  res.innerHTML += tabelaHTML;
 }
